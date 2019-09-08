b1c0539a2c60e1d83786788f9566a37b39b3c572
anovus <marwin.quito@gmail.com>
2019-09-07 23:59:21 -0500

    Added file via upload

---
+++ GenerateDirectoriesFromSourceFile.ps1
@@ -0,0 +1,18 @@
+﻿#Generate directories from a text file, with the name of each directory on separate lines in the file
+
+$directoriesFile = Read-Host -Prompt "Enter path with the directories file (Ex: C:\directories.txt) "
+$pattern=".+"
+$targetDir= Read-Host -Prompt 'Enter the target directory Here'
+
+$isAFile = (Get-Item $directoriesFile) -is [System.IO.FileInfo]
+
+if ($isAFile){
+
+get-content $directoriesFile | %{if($_ -match $pattern){mkdir "$targetDir\$_";}}
+
+}
+
+else{
+
+Write-Host "Directories .txt file was not specified" -ForegroundColor red -BackgroundColor black
+}
